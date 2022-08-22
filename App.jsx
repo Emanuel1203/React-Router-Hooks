ffbea980c041cb090bbbf4248d75e2c88b4def50
Emanuel Chudy <107444253+Emanuel1203@users.noreply.github.com>
2022-08-21 19:31:22 -0500

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeView from "./views/HomeView";
+import ToDoView from "./views/ToDoView";
+import CodewarsView from "./views/CodewarsView";
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomeView/>} />
+                    <Route path="to-do" element={<ToDoView />} />
+                    <Route path="codewars-compare" element={<CodewarsView />} />
+                    <Route path=":value" element={<Test />} />
+                </Routes>
+            </BrowserRouter>
+        );
+    }
+}   
+
+export default App;
